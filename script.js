50630f6e0a28e5f64249db40df3584687ba5d50c
DmitrijMK <dmitrijmk@gmail.com>
2018-06-22 10:32:41 +0300

    myApply / myCall

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
-// Singleton (практика Замыканий) =============================
-const Universe = (function universe() {
-  let instance;
-  this.size = 100;
+// 6. Полифилы
 
-  return function ConstructUniverse() {
-    if (instance) return instance;
-    if (this && this.constructor === ConstructUniverse) instance = this;
-    else return new ConstructUniverse();
-  };
-})();
+Object.prototype.myApply = function myApply(obj, value) {
+  return this.call(obj, ...value);
+};
 
-Universe.prototype.getSize = () => this.size;
-Universe.prototype.setSize = value => this.size = value;
+Object.prototype.myCall = function myCall(obj, ...value) {
+  return this.apply(obj, value);
+};
 
-const a = new Universe();
-const b = new Universe();
-console.log(a === b);
-console.log(a.getSize(), b.getSize()); // 100, 100
-a.setSize(200);
-console.log(a.getSize(), b.getSize()); // 200, 200
+const obj1 = {
+  a: 20,
+  foo(...numbers) {
+    return this.a + numbers.reduce((prev, curr) => prev + curr);
+  },
+};
 
+const obj2 = {
+  a: 30,
+};
+
+console.log(obj1.foo.myApply(obj2, [5, 5])); // 40
+console.log(obj1.foo.myApply(obj2, [5, 5, 10])); // 50
+console.log(obj1.foo.myCall(obj2, 5, 5, 20)); // 60
+console.log(obj1.foo.myCall(obj2, 5, 5, 10, 20)); // 70
+
+const f1 = obj1.foo.myBindByCall(obj2, 5, 5);
+console.log(f1()); // 40
+const f2 = obj1.foo.myBindByCall(obj2, 5, 5, 10);
+console.log(f2()); // 50
