a2b210f3db207db25fab61a6957c583ebe6b41a6
DmitrijMK <dmitrijmk@gmail.com>
2018-06-22 15:12:06 +0300

    4

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Промисы (практика промисов и асинхронности) ===========
 
 // 1 ==============================
-const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+const delay = ms => new Promise(resolve => setTimeout(resolve(100), ms));
 
 delay(1000).then(value => console.log('Done with ' + value)); // Done with 100
 
@@ -11,9 +11,9 @@ const secondUrl = 'http://www.json-generator.com/api/json/get/cfVGucaXPC';
 
 const httpGet = url => fetch(url).then(response => response.json());
 
-const getDataMethodPromise = (url1, url2) => httpGet(url1).
-  then(({getUsersData}) => getUsersData ? httpGet(url2).
-    then(result => console.log(result)) : console.error('error'));
+const getDataMethodPromise = (url1, url2) => httpGet(url1)
+  .then(({getUsersData}) => getUsersData ? httpGet(url2)
+    .then(result => console.log(result)) : console.error('error'));
 
 const getDataMethodAsyncAwait = async (url1, url2) =>
   (await httpGet(url1))['getUsersData'] ? console.log(await httpGet(url2))
@@ -23,8 +23,7 @@ getDataMethodAsyncAwait(firstUrl, secondUrl);
 getDataMethodPromise(firstUrl, secondUrl);
 
 // 3 ===================================
-
-const urls = [
+const urlsArr = [
   'http://www.json-generator.com/api/json/get/cevhxOsZnS',
   'http://www.json-generator.com/api/json/get/cguaPsRxAi',
   'http://www.json-generator.com/api/json/get/cfDZdmxnDm',
@@ -36,8 +35,8 @@ function makeRequestsConsistently(urls) {
   const results = [];
   let chain = Promise.resolve();
 
-  urls.forEach(url => chain = chain.then(() => httpGet(url)).
-    then(result => results.push(result)),
+  urls.forEach(url => chain = chain.then(() => httpGet(url))
+    .then(result => results.push(result)),
   );
 
   chain.then(() => console.log(results));
@@ -53,12 +52,19 @@ async function makeRequestsConsistently2(urls) {
   console.log(result);
 }
 
-const makeRequestsParallel = urls => Promise.all(urls.map(httpGet)).
-  then(result => console.log(result));
+const makeRequestsParallel = urls => Promise.all(urls.map(httpGet))
+  .then(result => console.log(result));
 
-makeRequestsConsistently(urls);
-makeRequestsConsistently2(urls);
-makeRequestsParallel(urls);
+makeRequestsConsistently(urlsArr);
+makeRequestsConsistently2(urlsArr);
+makeRequestsParallel(urlsArr);
 
 // 4 ===================================
 
+const getResolvedPromise = value =>
+  new Promise(resolve => resolve(value));
+
+getResolvedPromise(500)
+  .then((value) => if (value > 300){
+  throw new Error('Ошибка')
+    .catch(alert);
