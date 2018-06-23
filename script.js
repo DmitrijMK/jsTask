a4d5a79dd3bda544bbe2fd3e9cad512da3a8b1c3
DmitrijMK <dmitrijmk@gmail.com>
2018-06-23 14:31:25 +0300

    убраль лишнюю проверку

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 // 5. Наделение функционалом
-function duplicate() {
-  return this.length !== 0 ? [...this, ...this] : this;
+Array.prototype.duplicate = function duplicate() {
+  return [...this, ...this];
 };
 
-Array.prototype.duplicate = duplicate;
-
-console.log([1, 2, 3, 4].duplicate()); // [1, 2, 3, 4, 1, 2, 3, 4]
+console.log([].duplicate()); // [1, 2, 3, 4, 1, 2, 3, 4]
