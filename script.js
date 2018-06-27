e79e99e41cd24e70af327f087f12add170d6f648
DmitrijMK <dmitrijmk@gmail.com>
2018-06-27 15:53:11 +0300

    done Object.assign

--- script.js
+++ script.js
@@ -34,45 +34,75 @@
 // console.log(f2()); // 50
 
 // 2. Написать полифил на Object.create ===========================
-function objectCreate(proto, someObj) {
-  function F() {}
-
-  F.prototype = proto;
-  const f = new F();
-
-  return Object.defineProperties(f, someObj);
-}
-
-const obj = {
-  a: 1,
-};
-
-const obj2 = objectCreate(obj, {
-  p: {
-    value: 20,
-  },
-  k: {
-    value: 30,
-  },
-});
-
-console.log(obj2); // { p: 20, k: 30, __proto__: { a: 1 } }
+// function objectCreate(obj, propertiesObject) {
+//   function F() {}
+//
+//   F.prototype = obj;
+//   const f = new F();
+//
+//
+//   return Object.defineProperties(f, propertiesObject);
+// }
+//
+// const obj = {
+//   a: 1,
+// };
+//
+// const obj2 = objectCreate(obj, {
+//   p: {
+//     value: 20,
+//   },
+//   k: {
+//     value: 30,
+//   },
+// });
+//
+// console.log(obj2); // { p: 20, k: 30, __proto__: { a: 1 } }
 
 // 3 Написать свою реализацию new в виде функции myNew=========================
-function myNew(func) {
-  const that = Object.create(func.prototype);
-  func.apply(that);
-  return that;
+// function myNew(func) {
+//   const that = Object.create(func.prototype);
+//   func.apply(that);
+//   return that;
+// }
+//
+// function F() {
+//   this.a = 10;
+// }
+//
+// F.prototype.foo = function foo() {
+//   return this.a;
+// };
+//
+// const a = myNew(F);
+// console.log(a); // { a: 10, __proto__: { foo, constructor } }
+// console.log(a.foo()); // 10
+
+// 4. Написать полифил на Object.assign=========================
+function objectAssign(target, ...sources) {
+  if (!target) throw new TypeError('Can`t convert');
+
+  for (let i = 0; i < sources.length; i++) {
+    const nextSource = sources[i];
+    if (!nextSource) continue;
+
+    const keysArray = Object.keys(nextSource);
+    for (let i = 0; i < keysArray.length; i++) {
+      const desc = Object.getOwnPropertyDescriptor(nextSource, keysArray[i]);
+
+      if (desc !== undefined && desc.enumerable) {
+        target[keysArray[i]] = nextSource[keysArray[i]];
+      }
+    }
+  }
+  return target;
 }
 
-function F() {
-  this.a = 10;
-}
+const o2 = { b: 2, z: 5, f: null };
+const o3 = { c: 3 };
+const ar = [1];
+const df = null;
 
-F.prototype.foo = function foo() {
-  return this.a;
-};
-
-const a = myNew(F);
-console.log(a); // { a: 10, __proto__: { foo, constructor } }
-console.log(a.foo()); // 10
+const obj = objectAssign(ar, o2, o3, df);
+console.log(obj);
+console.log(ar);
